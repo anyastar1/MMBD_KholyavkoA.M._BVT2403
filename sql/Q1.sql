ab79d1dd599233a7fd9c260c546ef8a7f1710e62
Anna Kholyavko <miss.kholyavko2006@gmail.com>
2026-09-10 10:37:29 +0300

    Delete sql directory

--- sql/Q1.sql
+++
@@ -1,7 +0,0 @@
-select dc.federal_district, round(sum(f.amount_rub)) as turnover_rub
-from marts.fct_transactions f
-join marts.dim_date d    on f.date_sk = d.date_sk
-join marts.dim_client dc on f.client_sk = dc.client_sk
-where f.status = 'approved'and f.channel in ('pos', 'ecom') and d.date_actual between '2026-07-01' and '2026-07-31'
-group by 1
-order by 2 desc;
